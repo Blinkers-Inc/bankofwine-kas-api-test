4554c616ee53f9a6c53465752d0b109cf8d24a6d
dankimNLS <dan@nextlevelstudio.net>
2022-09-05 16:38:18 +0900

    First init

---
+++ src/constant.ts
@@ -0,0 +1,8 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
+export const headers = {
+  "Authorization": process.env.KAS_AUTHORIZATION!,
+  "x-chain-id": process.env.KAS_CHAIN_ID!,
+  "Content-Type": "application/json"
+}
